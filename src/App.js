6d5fd90cea073b980ceb640d5d7660e951c09b8d
Laura Padilla <lauvpadilla@gmail.com>
2020-09-27 16:51:31 -0400

    first comiittttttt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+);
+
+class App extends Component {
+  //with create react app, we can write our methods
+  // as arrow functions, meaning we don't need the
+  // constructor and bind
+
+  handleChange = (event) => console.log(event);
+
+  render() {
+    return (
+      <div className="page">
+        <Header />
+        <div className="search grid">
+          <input
+            className="input grid-item"
+            placeholder="Type something"
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
